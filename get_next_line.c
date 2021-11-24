9c91f20f928630eaaf2ef6dcf0cd7c6ddfda18b1
Ismail Raqi <raqiking@gmail.com>
2021-11-24 14:36:20 -0200

    la ilaha ila lah

--- get_next_line.c
+++ get_next_line.c
@@ -15,10 +15,10 @@
 char	*get_next_line(int fd)
 {
 	char	*str;
-	char 	*buf;
-	char	c;
+	static char	c;
 	int		j;
 	int		r;
+
 
 	j = 0;
 	if (BUFFER_SIZE <= 0)
@@ -29,14 +29,15 @@ char	*get_next_line(int fd)
 	while (j < BUFFER_SIZE || c != '\n')
 	{
 		r = read(fd, &c, 1);
+		if (r == 0)
+			break ;
 		if (r == -1)
 			return (free(str), NULL);
 		str[j++] = c;	
-		if (c == '\n' || r == 0)
+		if (c == '\n')
 			break ;
 	}
-	
-	if (r == 0)
+	if (r == 0 && j == 0)
 		return (NULL);
 	str[j] = '\0';
 	return (str);	
@@ -44,33 +45,31 @@ char	*get_next_line(int fd)
 
 int main(void)
 {
-	int	fd;
 	char	*str;
-	
-	fd = open("test.txt",O_RDONLY);
+	int	fd;
+	int	fd1;
 
-	// while (str)
-	// {
+	fd = open("test.txt",O_RDONLY);
+	fd1 = open("titi.txt", O_RDONLY);
 	str = get_next_line(fd);
 	printf("%s", str);
 	free(str);
-	
-	printf("\n");
-	
- 	str = get_next_line(fd);
+	str = get_next_line(fd1);
 	printf("%s", str);
 	free(str);
-	
-	printf("\n");
-	
- 	str = get_next_line(fd);
+	str = get_next_line(fd1);
 	printf("%s", str);
 	free(str);
-
-	// 	printf("//");
-	// 	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	// while (str)
+	// {
 	// 	str = get_next_line(fd);
-		
+
 	// }
 	return (0);
 }
